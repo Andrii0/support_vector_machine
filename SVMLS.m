0ee3dd33c6b880fbd10e76df7b02ef6a2bc8b461
Andrii0 <37821528+Andrii0@users.noreply.github.com>
2018-05-12 15:44:27 -0400

    Add files via upload

---
+++ SVMLS.m
@@ -0,0 +1,61 @@
+function output = SVMLS(x_data, y_data, unknown, transformation, gamma, a, b, varargin)
+    x_size = size(x_data, 2);
+    if not((x_size == size(y_data, 2)))
+        return    
+    end
+    if nargin < 4
+        transformation = 3; 
+        gamma = 1;
+        a = 1;
+        b = 1;
+    elseif nargin < 5
+        gamma = 1;
+        a = 1;
+        b = 1;
+    elseif nargin < 6
+        a = 1;
+        b = 1;
+    elseif nargin < 7 
+        b = 1;
+    end
+       
+
+    if (transformation == 1)
+        Fi = @(x, xx) dot(x, xx);
+    elseif (transformation == 2)
+        Fi = @(x, xx) (dot(x, xx) + 1)^a;
+    elseif (transformation == 3)
+        Fi = @(x, xx) exp(-(norm(x-xx)^2)/a^2);
+    elseif (transformation == 4)
+        Fi = @(x, xx) tanh(a*dot(x, xx)+b);
+    else
+        fprintf('error')
+    end
+
+    Omega = zeros(x_size, x_size);
+    for i = 1:x_size
+        for j = 1:x_size
+            Omega(i,j) = y_data(i)*y_data(j)*Fi(x_data(:,i), x_data(:,j));
+        end
+    end
+        
+    gamma_matrix = 1/gamma*eye(x_size);
+    Omega = Omega + gamma_matrix;
+        
+    A = [[0 y_data]' [-1.*y_data; Omega]];
+    o = [0; ones(x_size, 1)];
+    x = A\o;
+    
+    y = zeros(size(unknown,2), 1);
+    for j = 1:size(unknown, 2)
+        sum = 0;
+        for i = 1:x_size
+            sum = sum + x(i+1)*y_data(i)*Fi(x_data(:,i), unknown(:,j))+x(1);
+        end
+        
+        y(j) = sign(sum);
+    end
+    output = y;
+    
+
+end
---
+++ generate_data.m
@@ -0,0 +1,28 @@
+%function to generate new set of data
+function generate_data()
+    x1_cord = 3*rand(1, 20);
+    y1_cord = 3*rand(1, 20);
+    x2_cord = 3*rand(1, 20)+2;
+    y2_cord = 3*rand(1, 20)+2;
+    x_data = [[x1_cord; y1_cord] [x2_cord; y2_cord]];
+    y_data = [ones(1, 20) -ones(1,20)];
+
+    %%%create grid
+    unknown = [];
+    for i = 0:0.2:5
+        for j = 0:0.2:5
+            point = [i;j];
+            unknown = [unknown(:,:) point];
+        end
+    end
+    
+    assignin('base','training_data',x_data);
+    assignin('base','training_values',y_data);
+    assignin('base','testing_data',unknown);
+    %assignin('base','trans',3);
+    assignin('base','gamma',1);
+    assignin('base','a',0.7);
+    assignin('base','b',1);
+    
+    fprintf("new data generated\n")
+end
